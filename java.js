6fbf6275c4e6695cf23b0007e7882aeade2aad31
hokin2023 <hokin990@gmail.com>
2023-06-15 16:15:35 -0700

    added dark mode

---
+++ java.js
@@ -0,0 +1,31 @@
+var darkModeBtn = document.querySelector('.switch');
+var bodyElement = document.body;
+var containerElement = document.querySelector('main')
+var mode = localStorage.getItem('mode');
+var cardDescription = document.querySelectorAll('.card-reveal');
+// var cardDescription = Array.from.querySelector('.card-reveal')
+
+function toggleMode () {
+    console.log('yes');
+    bodyElement.classList.toggle('dark-mode');
+    containerElement.classList.toggle('dark-mode');
+
+    if (bodyElement.classList.contains('dark-mode')) {
+        localStorage.setItem('mode', 'dark');
+        darkModeBtn.textContent= 'Turn on Light Mode';
+    //    Array.from(cardDescription).style.color = "black";
+       for (var i = 0; i < cardDescription.length; i++) {
+        cardDescription[i].style.color = "black";
+       }
+
+    } else {
+        localStorage.setItem('mode', 'light');
+        darkModeBtn.textContent= 'Turn on Dark Mode';
+    }
+}
+
+
+
+
+darkModeBtn.addEventListener('click', toggleMode);
+
